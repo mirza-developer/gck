d0376c66f2fd46f710b9f87313905b6a12ed6d37
Alireza Tavakoli <43365143+mirza-developer@users.noreply.github.com>
2025-09-15 16:33:09 +0330

    Rebrand Gck Games to گیم سنتر کوثر and enhance UI

---
+++ Gck/wwwroot/js/neshan-map.js
@@ -0,0 +1,15 @@
+﻿window.initFooterMap = function () {
+    if (window.footerMapInitialized) return;
+    window.footerMapInitialized = true;
+    var mapDiv = document.getElementById('footer-map');
+    if (!mapDiv) return;
+    var map = new L.Map("footer-map", {
+        key: "web.d9e4389cc2e54a848a7c83d14a8535ec",
+        maptype: "neshan",
+        poi: false,
+        traffic: false,
+        center: [35.6837518, 51.3858216],
+        zoom: 14,
+    });
+    let marker = L.marker([35.6837518, 51.3858216]).addTo(map);
+};
