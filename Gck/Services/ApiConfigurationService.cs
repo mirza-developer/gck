69f19a3112a39084f9fd66debcdd98e53df48620
Alireza Tavakoli <43365143+mirza-developer@users.noreply.github.com>
2026-02-18 17:32:24 +0330

    Remove trailing slash from RELEASE BaseApiUrl

--- Gck/Services/ApiConfigurationService.cs
+++ Gck/Services/ApiConfigurationService.cs
@@ -3,7 +3,7 @@ namespace Gck.Services;
 public class ApiConfigurationService
 {
 #if RELEASE
-    public string BaseApiUrl { get; set; } = "https://api.gckgames.ir/";
+    public string BaseApiUrl { get; set; } = "https://api.gckgames.ir";
 #else
     public string BaseApiUrl { get; set; } = "http://localhost:5200";
 #endif
