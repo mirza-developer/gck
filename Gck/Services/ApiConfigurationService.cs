e64ce86d076c765bde2aa50f8274df56b0441cff
Alireza Tavakoli <43365143+mirza-developer@users.noreply.github.com>
2026-02-18 08:43:58 +0330

    Update RELEASE BaseApiUrl to use production domain

--- Gck/Services/ApiConfigurationService.cs
+++ Gck/Services/ApiConfigurationService.cs
@@ -3,7 +3,7 @@ namespace Gck.Services;
 public class ApiConfigurationService
 {
 #if RELEASE
-    public string BaseApiUrl { get; set; } = "http://185.141.213.163:8080/";
+    public string BaseApiUrl { get; set; } = "https://api.gckgames.ir/";
 #else
     public string BaseApiUrl { get; set; } = "http://localhost:5200";
 #endif
